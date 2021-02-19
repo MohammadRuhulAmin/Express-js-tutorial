662405e78e48bcf41e37042e244d049d0a887866
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-19 20:58:59 +0600

    router created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path =require('path');
 
 const app = new express();
 
@@ -7,4 +8,22 @@ const PORT = 5000 || process.env.PORT;
 
 app.listen(PORT,()=>{
     console.log(PORT + " HAS started! ");
+});
+
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+app.get('/index',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','index_2.html'));
+});
+
+app.get('/index/myInfo',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/myInfo','index_3.html'));
+});
+
+app.get('/myIndex/myotherInfo',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/myInfo','index_4.html'));
 });
