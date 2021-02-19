e5418e9b4c6e6237ee5da207567102c1ff693ca4
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-02-20 00:06:46 +0600

    attach static folder to use more than one time

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const express = require('express');
 const path    = require('path');
 const server = new express();
 
+
 const  PORT = process.env.PORT || 3000;
 server.listen(PORT,function(){
     console.log(`Server started at ${PORT}`);
 });
 
+
 server.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'myfiles','file_1.html'));
 });
+
 
 server.get('/index',(req,res)=>{
     res.sendFile(path.join(__dirname,'myfiles/aboutme','aboutme.html'));
 });
 
+
+
 server.get('/myIndex',function(req,res){
     res.sendFile(path.join(__dirname,'myInfo','x.html'));
 });
 
+server.use(express.static(path.join(__dirname,'public')));
+server.use(express.static(path.join(__dirname,'myFile')));
+server.get('/index/info',(req,res)=>{
+    res.sendFile(path.join(__dirname,'Folder1','myFile.html'));
+});
 
-
-
+server.use(express.static(path.join(__dirname,'AboutMyFolder')));
